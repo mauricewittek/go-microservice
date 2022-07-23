a77e07a20529075fad2d54b8a2e6c633ba16d91f
Maurice Wittek <maurice.wittek@gmail.com>
2022-07-23 10:19:40 +0200

    implements logging via rabbitmq in broker-service

---
+++ broker-service/event/event.go
@@ -0,0 +1,17 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func declareExchange(ch *amqp.Channel) error {
+	return ch.ExchangeDeclare(
+		"logs_topic", // name of exchange
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no wait
+		nil,          // arguments
+	)
+}
